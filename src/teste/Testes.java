5d91d7d59627d8f38fc52073f1b17d11a9f93c9d
Gabriel Braga <bragagabriel843@gmail.com>
2024-09-12 19:07:12 -0300

    teste inicial4

--- src/teste/Testes.java
+++ src/teste/Testes.java
@@ -4,5 +4,6 @@ public class Testes {
     public static void main(String[] args) {
         System.out.println("Iniciando testes");
         System.out.println("Iniciando testes2");
+        System.out.println("Iniciando testes3");
     }
 }
