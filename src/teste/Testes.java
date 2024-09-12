185ac2f8d186513009a106508e08328fbb6096f2
Gabriel Braga <bragagabriel843@gmail.com>
2024-09-12 18:54:50 -0300

    teste inicial2

--- src/teste/Testes.java
+++ src/teste/Testes.java
@@ -3,5 +3,6 @@ package teste;
 public class Testes {
     public static void main(String[] args) {
         System.out.println("Iniciando testes");
+        System.out.println("Iniciando testes2");
     }
 }
